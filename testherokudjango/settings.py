42eccd49a9f4e9d3e442e9d1d62d0d1f4694e5c6
syawal2kalii <syawal20162016@gmail.com>
2021-03-20 16:42:36 +0800

    p

--- testherokudjango/settings.py
+++ testherokudjango/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = 'images'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
